f29677108d2ffc20b732a3fb3bba6e6d16ca1cec
Aloosli <lizzwizz@tuta.io>
2023-03-12 09:59:17 +0000

    add homescreen & profilescreen

--- App.js
+++ App.js
@@ -1,23 +1,73 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
 
-import { View } from "react-native";
+import { Image, Statusbar } from "react-native";
 import LoginScreen from "./src/Screens/LoginScreen";
 import RegisterScreen from "./src/Screens/RegisterScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AuthenticatedUserProvider, {
+  AuthenticatedUserContext,
+} from "./Context/AuthenticationContext";
+import HomeScreen from "./src/Screens/HomeScreen";
+import ProfileScreen from "./src/Screens/ProfileScreen";
+import { useContext } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+const loadingGif = require("./assets/loading.gif");
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+function RootNavigator() {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} /> 
-      </Stack.Navigator>
+      {!user && isLoading === true ? (
+        <Image source={loadingGif} className="h-full w-full" />
+      ) : !user && isLoading === false ? (
+        <AuthStack />
+      ) : (
+        <MainStack />
+      )}
     </NavigationContainer>
+  );
+}
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Login"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+}
+function MainStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Spark" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
+}
+export default function App() {
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
   );
 }
---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import { View, Text, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { useLayoutEffect } from "react";
+const userAvatar = require("../../assets/man.png");
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+                    <Image source={userAvatar} className="h-10 w-10" />
+                </TouchableOpacity>
+            ),
+        });
+    }, [navigation]);
+
+
+    
+
+    
+  return (
+    <View>
+      <TouchableOpacity onPress={() => signOut(auth)}>
+        <Text>Home Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default HomeScreen;
